e0ba9fd19c6596a69cd0d941cdbf622b69d730bd
yi.ziqi <yi.ziqi@runyang.biz>
2023-11-22 18:36:10 +0800

    update rate model

--- src/components/LangSelector/index.vue
+++ src/components/LangSelector/index.vue
@@ -5,33 +5,33 @@
 </template>
 
 <script setup>
-import { ref } from "vue"
-const lang = ref("zh")
+import { ref } from 'vue'
+const lang = ref('zh')
 // zh（中文），en（英文），id（印尼），ph（菲律宾）
 const langOptions = ref([
   {
-    value: "zh",
-    label: "中文"
+    value: 'zh',
+    label: '中文'
   },
   {
-    value: "en",
-    label: "英文"
+    value: 'en',
+    label: '英文'
   },
   {
-    value: "id",
-    label: "印尼"
+    value: 'id',
+    label: '印尼'
   },
   {
-    value: "ph",
-    label: "菲律宾"
+    value: 'ph',
+    label: '菲律宾'
   }
 ])
 
-const emit = defineEmits(["change"])
+const emit = defineEmits(['change'])
 const changeHandler = (value) => {
-  emit("change", value)
+  emit('change', value)
 }
-emit("change", lang.value)
+emit('change', lang.value)
 </script>
 
 <style></style>
